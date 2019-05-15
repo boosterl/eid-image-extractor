bf3b3e5dfa86f1ae7f7f3b6aa681da4dbe307c9a
boosterl <bram.oosterlynck@gmail.com>
2019-05-15 18:19:20 +0200

    Add script

---
+++ eid-image-extractor.py
@@ -0,0 +1,37 @@
+from eidreader.main import eid2dict
+import base64
+
+
+def get_eid_data():
+    return eid2dict()
+
+
+def get_eid_image(eid_data):
+    return eid_data['PHOTO_FILE']
+
+
+def get_national_number(eid_data):
+    return eid_data['national_number']
+
+
+def eid_inserted(eid_data):
+    if (eid_data['success']):
+        return True
+    else:
+        return False
+
+
+def write_image(eid_image_decode, national_number):
+    eid_image = open('{}.jpeg'.format(national_number), 'wb')
+    eid_image.write(eid_image_decode)
+    eid_image.close()
+
+
+if __name__ == '__main__':
+    eid_data = get_eid_data()
+    if (eid_inserted(eid_data)):
+        eid_image_decode_string = base64.b64decode(get_eid_image(eid_data))
+        national_number = get_national_number(eid_data)
+        write_image(eid_image_decode_string, national_number)
+    else:
+        print('E-ID card not inserted, or could not be read')
