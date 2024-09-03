82aade690d5275c868bed078d0a7225e9cb08c1a
Bram Oosterlynck <bram.oosterlynck@gmail.com>
2024-09-03 20:19:55 +0200

    Restructure code

--- src/eid_image_extractor/__main__.py
+++ src/eid_image_extractor/__main__.py
@@ -1,7 +1,19 @@
-from eid_image_extractor import eid
+from eid_image_extractor.eid import Eid, EidNotInsertedException
+from eid_image_extractor import filesystem
+
 
 def main():
-    eid.extract_and_write_image()
+    try:
+        print("Reading E-ID data")
+        eid = Eid()
+        eid_image_decode_string = eid.get_decoded_eid_image()
+        national_number = eid.get_national_number()
+        print("Writing image from E-ID to file")
+        filesystem.write_file(eid_image_decode_string, f"{national_number}.jpg")
+        print("Done!")
+    except EidNotInsertedException:
+        print("E-ID card not inserted, or could not be read")
+
 
 if __name__ == "__main__":
     main()
