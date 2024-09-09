d9984fd9732c540b5bd193315e10c6edd62c4d01
Bram Oosterlynck <bram.oosterlynck@gmail.com>
2024-09-09 22:18:33 +0200

    Add some eye candy while waiting

--- src/eid_image_extractor/__main__.py
+++ src/eid_image_extractor/__main__.py
@@ -9,18 +9,18 @@ from eid_image_extractor.eid import Eid, EidNotInsertedException
 )
 def main(destination):
     try:
-        click.echo("Reading E-ID data")
+        click.echo("Reading E-ID data ⌛")
         eid = Eid()
         eid_image_decoded_string = eid.get_decoded_eid_image()
         destination_file = (
             destination if destination else f"{eid.get_national_number()}.jpg"
         )
-        click.echo(f"Writing image from E-ID to file {destination_file}")
+        click.echo(f"Writing image from E-ID to file {destination_file} ⌛")
         with click.open_file(destination_file, "wb") as f:
             f.write(eid_image_decoded_string)
-        click.echo(f"Done!")
+        click.echo(f"Done! ✅")
     except EidNotInsertedException:
-        print("E-ID card not inserted, or could not be read")
+        print("E-ID card not inserted, or could not be read ❌")
 
 
 if __name__ == "__main__":
