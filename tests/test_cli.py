4ec01f815681a2ae0d9e9ba3acbc4a48f2761d6f
Bram Oosterlynck <bram.oosterlynck@gmail.com>
2025-10-15 17:27:00 +0200

    Add initial CLI tests and test workflow

---
+++ tests/test_cli.py
@@ -0,0 +1,20 @@
+from click.testing import CliRunner
+from eid_image_extractor.__main__ import main
+
+def test_cli_help_flag():
+    runner = CliRunner()
+    result = runner.invoke(main, ["--help"])
+    assert result.exit_code == 0
+
+def test_cli_without_inserted_id():
+    runner = CliRunner()
+    result = runner.invoke(main, [])
+    assert "Reading E-ID data ⌛" in result.output
+    assert "E-ID card not inserted, or could not be read ❌" in result.output
+    assert result.exit_code == 1
+
+def test_cli_non_existing_argument():
+    runner = CliRunner()
+    result = runner.invoke(main, ["--no"])
+    assert result.exit_code == 2
+    
